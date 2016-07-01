613634282123f7a79be6235accea1b5e710a161c
Liang Zheng <jspop_no1@hotmail.com>
2016-07-01 19:42:56 +0800

    - diff files

--- script/unbroken.js
+++ script/unbroken.js
@@ -37,9 +37,76 @@ fs.writeFileSync('tmp.json', JSON.stringify(allFileInfo, null, 3))
 var fileIndex = utils.readJSONFile(config.fileIndexPath)
 
 var unbrokenUtils = {
-    diffFiles: function(fileIndex, allFileInfo) {
+    diffFiles: function (fileIndex, allFileInfo) {
         var tags = _.extend({}, fileIndex.tags)
+
+        // current path : fileInfo
+        var newPages = {}
+
+        // tag : fileInfo
+        var unknownPages = {}
+
+        // tag : 1 for all followings
+        var pathChanged = {}
+        var contentChanged = {}
+        var missingPages = {}
+
+        // tag : [...fileInfo]
+        var duplicatePages = {}
+        var duplicateChecks = {}
+
+        for (var path in allFileInfo) {
+            var fileInfo = allFileInfo[path]
+
+            if (!fileInfo.tag) {
+                // newPages
+                newPages[path] = fileInfo
+            } else {
+                if (duplicateChecks[fileInfo.tag]) {
+                    if (!duplicatePages[fileInfo.tag]) {
+                        duplicatePages[fileInfo.tag] = [duplicateChecks[fileInfo.tag]]
+                    }
+                    duplicatePages[fileInfo.tag].push(fileInfo)
+                }
+                else {
+                    duplicateChecks[fileInfo.tag] = fileInfo
+                }
+
+                if (fileInfo.tag in tags) {
+                    var prevInfo = fileIndex[tags[fileInfo.tag]]
+                    if (prevInfo.path != path) {
+                        pathChanged[fileInfo.tag] = 1
+                    }
+                    if (prevInfo.mtime < fileInfo.mtime) {
+                        contentChanged[fileInfo.tag] = 1
+                    }
+
+                    delete tags[fileInfo.tag]
+                } else {
+                    unknownPages[fileInfo.tag] = fileInfo
+                }
+            }
+        }
+
+
+        for (var tag in tags) {
+            missingPages[tag] = 1
+        }
+
+        return {
+            newPages: newPages,
+            unknownPages: unknownPages,
+
+            pathChanged: pathChanged,
+            contentChanged: contentChanged,
+            missingPages: missingPages,
+
+            duplicatePages: duplicatePages
+        }
     }
 }
 
-console.log(allFileInfo)
+var diff = unbrokenUtils.diffFiles(fileIndex, allFileInfo)
+
+
+console.log(allFileInfo, diff)
